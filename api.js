12c8f687247bc3179c0c3453537485b2d706d106
athelene <athelene@techinterpreter.com>
2021-03-02 13:07:52 -0600

    CORS 7

--- api.js
+++ api.js
@@ -8,6 +8,7 @@ var app = express();
 
 app.all('*', function(req, res, next) {
        res.header("Access-Control-Allow-Origin", "*");
+       res.header("Access-Control-Allow-Methods", "*");
        res.header("Access-Control-Allow-Headers", "X-Requested-With");
        res.header('Access-Control-Allow-Headers', 'Content-Type');
        next();
