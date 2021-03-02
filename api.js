e93098defa3af7c74223c7b33383b9d66e83e39a
athelene <athelene@techinterpreter.com>
2021-03-02 10:41:27 -0600

    cors5

--- api.js
+++ api.js
@@ -5,9 +5,14 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var cors = require('cors');
 var app = express();
-app.use(cors());
+//app.use(cors());
 var router = express.Router();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "localhost:8080"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
