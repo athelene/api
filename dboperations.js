4644cc001ac7d50a3546f5c8358a2e60aeed2b08
athelene <athelene@techinterpreter.com>
2021-03-06 12:48:29 -0600

    mar 6 update- just prior to inmotion 1st upload

--- dboperations.js
+++ dboperations.js
@@ -3,8 +3,9 @@ const sql = require('mssql');
 
 async function getStories(){
     try{
+        console.log('about to connect to sql');
         let pool = await sql.connect(config);
-        let stories = await pool.request().query("SELECT * from StoryTbl");
+        let stories = await pool.request().query("SELECT StoryID, StoryTitle, FORMAT (StoryDate, 'MMM dd yyyy hh:mm:ss tt') as StoryDate from StoryTbl");
         return stories.recordsets;
     }
     catch (error) {
@@ -13,6 +14,33 @@ async function getStories(){
 
 }
 
+async function getStories(){
+    try{
+        console.log('about to connect to sql');
+        let pool = await sql.connect(config);
+        let stories = await pool.request().query("SELECT StoryID, StoryTitle, FORMAT (StoryDate, 'MMM dd yyyy hh:mm:ss tt') as StoryDate from StoryTbl");
+        return stories.recordsets;
+    }
+    catch (error) {
+        console.log(error);
+    }
+
+}
+
+async function getTodaysQC(){
+    try{
+        console.log('about to connect to sql');
+        let pool = await sql.connect(config);
+        let todaysqc = await pool.request().query("SELECT ViewPointQ, FORMAT (ViewPointDate, 'MM-dd-yyyy') as ViewPointDate from ViewPointTbl where ViewPointDate = (CONVERT(date, getdate()))");
+        return todaysqc.recordsets;
+    }
+    catch (error) {
+        console.log(error);
+    }
+
+}
+
 module.exports = {
-    getStories : getStories
+    getStories : getStories,
+    getTodaysQC : getTodaysQC
 }
